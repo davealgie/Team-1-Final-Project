60a4c471766795140cbcf49ca46d0cee8e3bbb04
George <Gwhyte@qw.com>
2021-08-25 19:38:25 +0100

    ooh backend unit tests crud functional, might need looking at again later

--- src/main/resources/test-schema.sql
+++ src/main/resources/test-schema.sql
@@ -1,6 +1,7 @@
 
- 
-CREATE TABLE `albums` (
+
+
+CREATE TABLE IF NOT EXISTS`albums` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `cover` varchar(45) DEFAULT NULL,
@@ -9,19 +10,18 @@ CREATE TABLE `albums` (
   UNIQUE KEY `name` (`name`),
   KEY `artist_id_idx` (`artist_id`),
   CONSTRAINT `artist_id` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) 
+)
 
 
-
-CREATE TABLE `artists` (
+CREATE TABLE IF NOT EXISTS `artists` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-)
+) 
 
 
-CREATE TABLE `genres` (
+CREATE TABLE IF NOT EXISTS `genres` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `description` varchar(250) NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE `genres` (
   UNIQUE KEY `description_UNIQUE` (`description`)
 ) 
 
-CREATE TABLE `playlists` (
+CREATE TABLE IF NOT EXISTS `playlists` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `user_id` bigint NOT NULL,
@@ -43,8 +43,7 @@ CREATE TABLE `playlists` (
   KEY `user_id_idx` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) 
-
-CREATE TABLE `playlists_tracks` (
+CREATE TABLE IF NOT EXISTS `playlists_tracks` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `playlist_id` bigint NOT NULL,
   `track_id` bigint NOT NULL,
@@ -55,7 +54,7 @@ CREATE TABLE `playlists_tracks` (
   CONSTRAINT `track_id` FOREIGN KEY (`track_id`) REFERENCES `tracks` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) 
 
-CREATE TABLE `tracks` (
+CREATE TABLE IF NOT EXISTS `tracks` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `duration` int DEFAULT NULL,
@@ -68,13 +67,13 @@ CREATE TABLE `tracks` (
   KEY `genre_id_idx` (`genre_id`),
   CONSTRAINT `album_id` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `genre_id` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`) ON UPDATE CASCADE
-)
+) 
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `full_name` varchar(45) NOT NULL,
   `user_name` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-)
+) 
