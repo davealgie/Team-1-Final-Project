e6fd990976012f8f17369012a9c56acd5e67c69d
George <Gwhyte@qw.com>
2021-08-27 11:30:25 +0100

    conflicts

--- src/main/java/com/qa/choonz/persistence/domain/Artist.java
+++ src/main/java/com/qa/choonz/persistence/domain/Artist.java
@@ -1,7 +1,11 @@
 package com.qa.choonz.persistence.domain;
 
+ 
+
 import java.util.List;
 import java.util.Objects;
+
+ 
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -13,105 +17,23 @@ import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-<<<<<<< HEAD
-@Entity
-public class Artist {
+ 
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-
-	@NotNull
-	@Size(max = 100)
-	@Column(unique = true)
-	private String name;
-
-	@OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
-	private List<Album> albums;
-
-	public Artist() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Artist(long id, @NotNull @Size(max = 100) String name, List<Album> albums) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.albums = albums;
-	}
-
-	public Artist(long id, @NotNull @Size(max = 100) String name) {
-		super();
-		this.id = id;
-		this.name = name;
-
-	}
-
-	public Artist(String name) {
-		super();
-		this.name = name;
-
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<Album> getAlbums() {
-		return albums;
-	}
-
-	public void setAlbums(List<Album> albums) {
-		this.albums = albums;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Artist [id=").append(id).append(", name=").append(name).append(", albums=").append(albums)
-				.append("]");
-		return builder.toString();
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(albums, id, name);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Artist)) {
-			return false;
-		}
-		Artist other = (Artist) obj;
-		return Objects.equals(albums, other.albums) && id == other.id && Objects.equals(name, other.name);
-	}
-=======
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+ 
+
 
 @Entity
 public class Artist {
+
+ 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+ 
 
     @NotNull
     @Size(max = 100)
@@ -122,10 +44,14 @@ public class Artist {
     @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
     private List<Album> albums;
 
+ 
+
     public Artist() {
         super();
         // TODO Auto-generated constructor stub
     }
+
+ 
 
     public Artist(long id, @NotNull @Size(max = 100) String name, List<Album> albums) {
         super();
@@ -134,29 +60,43 @@ public class Artist {
         this.albums = albums;
     }
 
+ 
+
     public long getId() {
         return id;
     }
+
+ 
 
     public void setId(long id) {
         this.id = id;
     }
 
+ 
+
     public String getName() {
         return name;
     }
+
+ 
 
     public void setName(String name) {
         this.name = name;
     }
 
+ 
+
     public List<Album> getAlbums() {
         return albums;
     }
 
+ 
+
     public void setAlbums(List<Album> albums) {
         this.albums = albums;
     }
+
+ 
 
     @Override
     public String toString() {
@@ -166,10 +106,14 @@ public class Artist {
         return builder.toString();
     }
 
+ 
+
     @Override
     public int hashCode() {
         return Objects.hash(albums, id, name);
     }
+
+ 
 
     @Override
     public boolean equals(Object obj) {
@@ -182,6 +126,8 @@ public class Artist {
         Artist other = (Artist) obj;
         return Objects.equals(albums, other.albums) && id == other.id && Objects.equals(name, other.name);
     }
->>>>>>> 6ff8bfe74a1b92aea4aca058229be70e182c9eb5
+
+ 
 
 }
+
