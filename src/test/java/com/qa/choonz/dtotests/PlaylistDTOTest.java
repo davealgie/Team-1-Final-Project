0bce5edd3cdbd267c183b0ce12b468eb293e11d2
George-git1 <85874668+George-git1@users.noreply.github.com>
2021-08-30 21:27:39 +0100

    changes to dto

--- src/test/java/com/qa/choonz/dtotests/PlaylistDTOTest.java
+++ src/test/java/com/qa/choonz/dtotests/PlaylistDTOTest.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.Test;
 import com.qa.choonz.rest.dto.PlaylistDTO;
 
 public class PlaylistDTOTest {
-	private PlaylistDTO playlistDTO = new PlaylistDTO(0L, "name", "description", "artwork");
+	private PlaylistDTO playlistDTO = new PlaylistDTO(0L, "name", "description", "artwork", new ArrayList<>(), "users");
 
 	@Test
 	public void PlaylistTest() {
 	
-		assertThat("PlaylistDTO [id=0, name=name, description=description, artwork=artwork, tracks=[], users=null".toString()+"]").isEqualTo(playlistDTO.toString());
+		assertThat("PlaylistDTO [id=0, name=name, description=description, artwork=artwork, tracks=[], users=users".toString()+"]").isEqualTo(playlistDTO.toString());
+
 }
 }
--- src/test/java/com/qa/choonz/unittests/PlaylistServiceTest.java
+++ src/test/java/com/qa/choonz/unittests/PlaylistServiceTest.java
@@ -20,77 +20,73 @@ import com.qa.choonz.service.PlaylistService;
 
 @SpringBootTest
 public class PlaylistServiceTest {
-	
+
 	@MockBean
 	private PlaylistRepository repo;
-	
+
 	@MockBean
 	private UserRepository userRepo;
-	
+
 	@Autowired
 	private PlaylistService service;
-	
-	
+
 	private Optional<Users> optionalUser = Optional.of(new Users(1L, "fullname", "username", "password"));
-	
+
 	private Playlist playlist = new Playlist(1L, "name", "description", "artwork", new ArrayList<>());
-	
+
 	private PlaylistDTO playlistDTO = new PlaylistDTO(1L, "name", "description", "artwork");
-	
-	private Optional<Playlist> optionalPlaylist = Optional.of(new Playlist(1L, "name", "description", "artwork", new ArrayList<>()));
-	
-	private Playlist updatedPlaylist = new Playlist(1L, "updated name", "description", "artwork", new ArrayList<>());
-	
-<<<<<<< HEAD
-	private PlaylistDTO updatedPlaylistDTO = new PlaylistDTO(1L, "updated name", "description", "artwork",new ArrayList<>());
-=======
+
+	private Optional<Playlist> optionalPlaylist = Optional
+			.of(new Playlist(1L, "name", "description", "artwork", new ArrayList<>()));
+
+	private Playlist updatedPlaylist = new Playlist(1L, "updated name", "description", "artwork", tracks, users);
+
 	private PlaylistDTO updatedPlaylistDTO = new PlaylistDTO(1L, "updated name", "description", "artwork");
->>>>>>> f06a13250daad75a45b3cefa3bf84cf9fb9a4cec
-	
+
 	@Test
 	public void testCreate() {
-		
+
 		Mockito.when(this.userRepo.findById(1L)).thenReturn(optionalUser);
-		
+
 		Mockito.when(this.repo.save(playlist)).thenReturn(playlist);
-		
+
 		assertThat(playlistDTO).isEqualTo(this.service.create(playlist));
-		
+
 		Mockito.verify(this.repo, Mockito.times(1)).save(playlist);
 	}
-	
+
 	@Test
 	public void testReadAll() {
-		
+
 		Mockito.when(this.repo.findAll()).thenReturn(new ArrayList<>());
-		
+
 		assertThat(new ArrayList<>()).isEqualTo(this.service.read());
-		
+
 		Mockito.verify(this.repo, Mockito.times(1)).findAll();
 	}
-	
+
 	@Test
 	public void testReadById() {
 		Mockito.when(this.repo.findById(1L)).thenReturn(optionalPlaylist);
-		
+
 		assertThat(playlistDTO).isEqualTo(this.service.read(1L));
-		
+
 		Mockito.verify(this.repo, Mockito.times(1)).findById(1L);
 	}
-	
+
 	@Test
 	public void testUpdate() {
 		Mockito.when(this.repo.findById(1L)).thenReturn(optionalPlaylist);
-		
+
 		Mockito.when(this.repo.save(updatedPlaylist)).thenReturn(updatedPlaylist);
-		
+
 		assertThat(updatedPlaylistDTO).isEqualTo(this.service.update(updatedPlaylist, 1L));
-		
+
 		Mockito.verify(this.repo, Mockito.times(1)).findById(1L);
-		
+
 		Mockito.verify(this.repo, Mockito.times(1)).save(updatedPlaylist);
 	}
-	
+
 	@Test
 	public void testDelete() {
 		assertThat(true).isEqualTo(this.service.delete(1L));
