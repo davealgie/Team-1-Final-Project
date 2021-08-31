de8c9f3c9948dad522b4826e527343c6de5246ad
davealgie <davidalgie@hotmail.co.uk>
2021-08-31 10:08:56 +0100

    Updates to card look

--- src/main/resources/static/js/album.js
+++ src/main/resources/static/js/album.js
@@ -61,6 +61,47 @@
       return div;
     }
 
+//loops through and extracts data from json, assigns it to variables
+function cardData(dataData){
+  for (let dataRecord of dataData){
+      singleIterationCheck = 0;
+      for (value in dataRecord){
+          if (typeof dataRecord[value] === 'object'){
+            if (singleIterationCheck != 0){
+            } else {
+              let id = dataRecord.id;
+              let image = dataRecord.cover;
+              let title = dataRecord.name;
+              let description = dataRecord.artist.name;
+              let buttonText = "View";
+              let buttonLink = "albumview.html?id="+id;
+              let artistLink = "artistalbums.html?id=" + dataRecord.artist.id;
+              createCard(id, image, title, description, buttonText, buttonLink, artistLink);
+              singleIterationCheck++;
+            }
+          }
+      }
+  }
+}
+
+//clones a hidden html card, attaches data, and appends to html
+function createCard(id, image, title, description, buttonText, buttonLink, artistLink){
+  let cards = document.querySelector("div.showcards");
+  let cloneCard = document.querySelector("div.card").cloneNode(true);
+  cloneCard.id = ("card" + id);
+  cloneCard.querySelector("img").src=(image);
+  cloneCard.querySelector("#imagelink").href=(buttonLink);
+  cloneCard.querySelector("#titlelink").innerHTML = (title);
+  cloneCard.querySelector("#titlelink").href = (buttonLink);
+  cloneCard.querySelector("#textlink").innerHTML = (description);
+  cloneCard.querySelector("#textlink").href = (artistLink);
+  cloneCard.querySelector("#button").innerHTML = (buttonText);
+  cloneCard.querySelector("#button").href = (buttonLink);
+  cards.appendChild(cloneCard);
+}
+
+ 
+
     function simpleFetch(){
         const a = ["http://localhost:81/albums/read"];
             fetch(a)
